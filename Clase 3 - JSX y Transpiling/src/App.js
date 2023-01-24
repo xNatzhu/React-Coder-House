a78be94c30bc830b2cac2b30d783140b9b69a700
Agustin Saravia <87948201+xNatzhu@users.noreply.github.com>
2023-01-23 22:23:27 -0300

    Add files via upload

---
+++ Clase 3 - JSX y Transpiling/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './Componentes/Menu/Menu.js';
+function App() {
+  return (
+    <body>
+      <Menu></Menu>
+      <main>
+        <section>
+        </section>
+      </main>
+    </body>
+  );
+}
+
+export default App;
+ 
